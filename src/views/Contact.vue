a042445ce73119802722f8d54d90253b00bd30f3
IsabellaSchoen <is391sch@htwg-konstanz.de>
2021-08-25 12:39:49 +0200

    started with basics for form groups

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,8 +1,32 @@
 <template>
-  <div class="contact">
-    <h1>this is a contact page</h1>
+  <div>
+    <b-form-group label-cols="4" label-cols-lg="2" label-size="lg" label-for="input-default">
+      <b-form-input v-model="text" placeholder="Vorname"></b-form-input>
+    </b-form-group>
+
+    <b-form-group label-cols="4" label-cols-lg="2" label-size="lg" label-for="input-default">
+      <b-form-input v-model="text" placeholder="Nachname"></b-form-input>
+    </b-form-group>
+
+    <b-form-group label-cols="4" label-cols-lg="2" label-size="lg" label-for="input-default">
+    <b-form-input v-model="text" placeholder="EMail"></b-form-input>
+    </b-form-group>
+
+    <b-form-group label-cols="4" label-cols-lg="2" label-size="lg" label-for="input-default">
+      <b-form-input v-model="text" placeholder="Text"></b-form-input>
+    </b-form-group>
+
+
+    <div class="button">
+      <b-button variant="secondary">
+        Senden
+      </b-button>
+    </div>
+
   </div>
 </template>
+
+
 
 <script>
 export default {
